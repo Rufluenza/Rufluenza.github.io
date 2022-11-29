cfcaed5b4098aad584c570d5dbad01597d4865e5
Rufluenza <NEXT5177@edu.nextkbh.dk>
2022-11-29 16:06:25 +0100

    update if by-label exists

--- website.py
+++ website.py
@@ -1,35 +1,37 @@
 import streamlit as st
 import os
 
+
+password = 'password'
+# /dev/disk/by-label
 dev = {}
 devs = {}
 def get_usb():
-    i = 0      
-    for dev in os.listdir('/dev/disk/by-label'):
-        if dev != "ESP" and dev != "EFI":
-            i += 1
-            l = '/dev/disk/by-label/'+dev
-            devs[i] = l
+    i = 0
+    if os.path.exists('/dev/disk/by-label'):
+        for dev in os.listdir('/dev/disk/by-label'):
+            if dev != "ESP" and dev != "EFI":
+                i += 1
+                dev[i] = '/dev/disk/by-label/'+dev
 
     return devs
 
 
+
 st.header("Xmount eject")
 st.subheader("Eject your USB devices")
-def main():
-    for i in range(len(get_usb())):
-        l = str(i)
-        st.write(get_usb()[i+1].split('/')[-1])
-        st.container()
-        l = st.button('Eject ' + 'Nr. '+l+' ' + get_usb()[i+1].split('/')[-1])
-        if l:
-            st.write('ejecting'+ ' ' + get_usb()[i+1])
-            os.system('udisksctl unmount --block-device '+get_usb()[i+1])
-            if os.system('udisksctl power-off --block-device '+get_usb()[i+1]) == 0:
-                st.write('Ejected'+ ' ' + get_usb()[i+1].split('/')[-1])
-                st.write('Reload the page to see the changes.  ')
-                # refresh the page from the server side
 
-    
+def main():
+
+    for i in range(len(get_usb())):
+        # all_names = get_name()[i]
+        st.write(get_usb()[i+1].split('/')[-1])
+        st.container() # make a container for each device
+        if st.button('Eject' + ' ' + get_usb()[i+1].split('/')[-1]):
+            st.write('ejecting'+ ' ' + get_usb()[i+1])
+            if os.system('echo '+password+' | sudo -S umount '+get_usb()[i+1]) == 0: # if the command is successful maybe delete the if statement "sudo -S umount" on linux
+                st.write('Ejected'+ ' ' + get_usb()[i+1].split('/')[-1])
+                st.write('Reload the page to see the changes')
+
 if __name__ == "__main__":
     main()
